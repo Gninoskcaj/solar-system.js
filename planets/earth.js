7780631a1b6a940861ada690cf3484a8596361fe
Jackson <jackson.mooring@gmail.com>
2019-06-17 17:04:44 -0500

    Delete render function in render.js

--- planets/earth.js
+++ planets/earth.js
@@ -11,20 +11,3 @@ let material = new THREE.MeshStandardMaterial({
 let circle = new THREE.Mesh(geometry, material);
 scene.add(circle);
 circle.rotation.x -= .6;
-
-
-let render = function () {
-	let time = Date.now()
-
-	requestAnimationFrame(render);
-
-	circle.position.x = Math.cos(time * 0.001) * 10;
-	circle.position.y = Math.sin(time * 0.001) * 10;
-	circle.position.z = Math.sin(time * 0.001) * 10;
-
-	circle.rotation.y += .05;
-	renderer.render(scene, camera);
-};
-
-render();
-
