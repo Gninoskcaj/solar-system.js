d75c0261e31cde194d1a70254346e746d01a4b66
Jackson <jackson.mooring@gmail.com>
2019-06-17 09:38:03 -0500

    Fix a bug

--- planets/earth.js
+++ planets/earth.js
@@ -1,20 +1,31 @@
 
-let texture = new THREE.TextureLoader().load('https://space-assets-1.s3.us-east-2.amazonaws.com/land_ocean_ice_cloud_2048.jpg');
 
-let geometry = new THREE.SphereBufferGeometry(2, 32, 16);
-let material = new THREE.MeshBasicMaterial({ flatShading: false, shininess: 10, wireframe: false, map: texture });
-let circle = new THREE.Mesh(geometry, material);
+const THREE = require('three')
+// #1
+var scene = new THREE.Scene();
+var aspect = window.innerWidth / window.innerHeight;
+var camera = new THREE.PerspectiveCamera(75, aspect, 0.1, 1000);
+var renderer = new THREE.WebGLRenderer();
+renderer.setSize(window.innerWidth, window.innerHeight);
+document.body.appendChild(renderer.domElement);
+
+// #2
+var texture = new THREE.TextureLoader().load('https://space-assets-1.s3.us-east-2.amazonaws.com/land_ocean_ice_cloud_2048.jpg');
+
+var geometry = new THREE.SphereBufferGeometry(2, 32, 16);
+var material = new THREE.MeshBasicMaterial({ flatShading: false, shininess: 10, wireframe: false, map: texture });
+var circle = new THREE.Mesh(geometry, material);
 scene.add(circle);
 
 scene.add(circle);
 camera.position.z = 5;
-let controls = new THREE.OrbitControls(camera, renderer.domElement);
+var controls = new THREE.OrbitControls(camera, renderer.domElement);
 
 // #3
-let render = function () {
+var render = function () {
 	requestAnimationFrame(render);
-	// 			circle.rotation.x += .01;
-	// 			circle.rotation.y += .01;
+	circle.rotation.x += .01;
+	circle.rotation.y += .01;
 	renderer.render(scene, camera);
 };
 
