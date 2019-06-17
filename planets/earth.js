cb5cebfe6d17c60eefc06a7043e1274847848326
Jackson <jackson.mooring@gmail.com>
2019-06-17 15:49:35 -0500

    Update earth.js

--- planets/earth.js
+++ planets/earth.js
@@ -1,12 +1,4 @@
-// #1
-let scene = new THREE.Scene();
-let aspect = window.innerWidth / window.innerHeight;
-let camera = new THREE.PerspectiveCamera(75, aspect, 0.1, 1000);
-let renderer = new THREE.WebGLRenderer();
-renderer.setSize(window.innerWidth, window.innerHeight);
-document.body.appendChild(renderer.domElement);
 
-// #2
 let texture = new THREE.TextureLoader().load(
 	'https://space-assets-1.s3.us-east-2.amazonaws.com/land_ocean_ice_cloud_2048.jpg');
 
@@ -31,7 +23,6 @@ scene.add(bulbLight);
 camera.position.z = 12;
 camera.position.x = 17;
 camera.position.z = -12;
-let controls = new THREE.OrbitControls(camera, renderer.domElement);
 circle.rotation.x -= .6;
 // #3
 let render = function () {
