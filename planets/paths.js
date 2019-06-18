62df68ee09912c190031ca56f21ba8f3690e357f
Jackson <jackson.mooring@gmail.com>
2019-06-17 19:33:02 -0500

    Functionize paths.js

--- planets/paths.js
+++ planets/paths.js
@@ -1,4 +1,7 @@
-var earthPathGeo = new THREE.CircleGeometry( 5, 32 );
-var earthPathMater = new THREE.MeshBasicMaterial( { color: 0xffff00 } );
-var circle = new THREE.Mesh( geometry, material );
-scene.add( circle );
+
+function paths() {
+	var earthPathGeo = new THREE.CircleGeometry(5, 32);
+	var earthPathMater = new THREE.MeshBasicMaterial({ color: 0xffff00 });
+	var circle = new THREE.Mesh(geometry, material);
+	scene.add(circle);
+}
