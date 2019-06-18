1bddaf82174f7732e43493fc16de3f501969f005
Jackson <jackson.mooring@gmail.com>
2019-06-17 19:37:51 -0500

    Create mars.js

---
+++ planets/mars.js
@@ -0,0 +1,17 @@
+function mars() {
+
+
+	let texture = new THREE.TextureLoader().load(
+		'https://space-assets-1.s3.us-east-2.amazonaws.com/land_ocean_ice_cloud_2048.jpg');
+
+	let geometry = new THREE.SphereBufferGeometry(2, 32, 16);
+	let material = new THREE.MeshStandardMaterial({
+		flatShading: false,
+		wireframe: false,
+		map: texture,
+	});
+	let mars = new THREE.Mesh(geometry, material);
+	scene.add(mars);
+	mars.rotation.x -= .6;
+}
+mars()
