02c4bd514c4db19723c2fa6bda627d5ea9423ca9
Jackson <jackson.mooring@gmail.com>
2019-06-18 17:28:16 -0500

    Update earth textures

--- planets/earth.js
+++ planets/earth.js
@@ -1,15 +1,24 @@
 
-	let earthColorMap = new THREE.TextureLoader().load(
-		'https://space-assets-1.s3.us-east-2.amazonaws.com/land_ocean_ice_cloud_2048.jpg');
+let earthColorMap = new THREE.TextureLoader().load(
+	'https://space-assets-1.s3.us-east-2.amazonaws.com/earth.jpg'
+);
 
-	let earthGeo = new THREE.SphereBufferGeometry(2, 32, 16);
-	let earthStandardMater = new THREE.MeshStandardMaterial({
-		flatShading: false,
-		wireframe: false,
-		// normalMap: earthNormalMap,
-		// metalnessMap: earthMetalnessMap,
-		map: earthColorMap,
-	});
-	let earth = new THREE.Mesh(earthGeo, earthStandardMater);
-	scene.add(earth);
-	earth.rotation.x -= .6;
+let earthNormalMap = new THREE.TextureLoader().load(
+	'https://space-assets-1.s3.us-east-2.amazonaws.com/earthTexture.jpg'
+);
+
+let earthMetalnessMap = new THREE.TextureLoader().load(
+	'https://space-assets-1.s3.us-east-2.amazonaws.com/earthSpecular.jpg'
+)
+
+let earthGeo = new THREE.SphereBufferGeometry(2, 32, 16);
+let earthStandardMater = new THREE.MeshStandardMaterial({
+	flatShading: false,
+	wireframe: false,
+	normalMap: earthNormalMap,
+	metalnessMap: earthMetalnessMap,
+	map: earthColorMap,
+});
+let earth = new THREE.Mesh(earthGeo, earthStandardMater);
+scene.add(earth);
+earth.rotation.x -= .6;
