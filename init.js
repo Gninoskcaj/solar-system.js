a4da5d251af71f6461ff6a6e66f99480d85adaa3
Jackson <jackson.mooring@gmail.com>
2019-06-17 19:57:19 -0500

    Create mars

--- init.js
+++ init.js
@@ -1,4 +1,4 @@
-function init() {
+
 	let scene = new THREE.Scene();
 	let aspect = window.innerWidth / window.innerHeight;
 	let camera = new THREE.PerspectiveCamera(75, aspect, 0.1, 2000);
@@ -9,6 +9,4 @@ function init() {
 	camera.position.z = 12;
 	camera.position.x = 20;
 	camera.position.z = 12;
-}
 
-init()
--- planets/mars.js
+++ planets/mars.js
@@ -1,11 +1,8 @@
-function mars() {
-
-
-	let texture = new THREE.TextureLoader().load(
+	let marsTexture = new THREE.TextureLoader().load(
 		'https://space-assets-1.s3.us-east-2.amazonaws.com/land_ocean_ice_cloud_2048.jpg');
 
-	let geometry = new THREE.SphereBufferGeometry(2, 32, 16);
-	let material = new THREE.MeshStandardMaterial({
+	let marsGeometry = new THREE.SphereBufferGeometry(10, 32, 16);
+	let marsMaterial = new THREE.MeshBasicMaterial({
 		flatShading: false,
 		wireframe: false,
 		map: texture,
@@ -13,5 +10,3 @@ function mars() {
 	let mars = new THREE.Mesh(geometry, material);
 	scene.add(mars);
 	mars.rotation.x -= .6;
-}
-mars()
