45795a2c1c9cb4f14d1ad84f457676f0fcc47f33
Jackson <jackson.mooring@gmail.com>
2019-06-18 18:55:07 -0500

    Update mars vars

--- planets/mars.js
+++ planets/mars.js
@@ -1,11 +1,11 @@
 	let marsColorMap = new THREE.TextureLoader().load(
 		'https://space-assets-1.s3.us-east-2.amazonaws.com/mars.jpg');
 
-	let marsGeometry = new THREE.SphereBufferGeometry(1, 32, 16);
-	let marsMaterial = new THREE.MeshStandardMaterial({
+	let marsGeo = new THREE.SphereBufferGeometry(1, 32, 16);
+	let marsStandardMater = new THREE.MeshStandardMaterial({
 		flatShading: false,
 		wireframe: false,
-		map: marsTexture,
+		map: marsColorMap,
 	});
-	let mars = new THREE.Mesh(marsGeometry, marsMaterial);
+	let mars = new THREE.Mesh(marsGeo, marsStandatdMater);
 	scene.add(mars);
