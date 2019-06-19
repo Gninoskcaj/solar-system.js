d199244b1d7c2f8f99fe78bd201c4697e91160ea
Jackson <jackson.mooring@gmail.com>
2019-06-19 16:00:17 -0500

    Make satern BIG

--- planets/satern/satern.js
+++ planets/satern/satern.js
@@ -2,11 +2,12 @@ let saternColorMap = new THREE.TextureLoader().load(
 	'https://space-assets-1.s3.us-east-2.amazonaws.com/earth.jpg'
 );
 
-let saternGeo = new THREE.SphereBufferGeometry(2, 32, 16);
+let saternGeo = new THREE.SphereBufferGeometry(10, 32, 16);
 let saternStandardMater = new THREE.MeshStandardMaterial({
 	flatShading: false,
 	wireframe: false,
 	map: saternColorMap,
 });
 let satern = new THREE.Mesh(saternGeo, saternStandardMater);
+
 scene.add(satern);
