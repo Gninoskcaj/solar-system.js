75d899805e6b383233030c57f29ce6b48f583a08
Jackson <jackson.mooring@gmail.com>
2019-06-19 14:04:59 -0500

    Make jupiter big

--- planets/jupiter.js
+++ planets/jupiter.js
@@ -1,13 +1,14 @@
 let jupiterColorMap = new THREE.TextureLoader().load(
-	'https://space-assets-1.s3.us-east-2.amazonaws.com/earth.jpg'
+	'https://space-assets-1.s3.us-east-2.amazonaws.com/jupiter.jpg'
 );
 
 
-let jupiterGeo = new THREE.SphereBufferGeometry(2, 32, 16);
+let jupiterGeo = new THREE.SphereBufferGeometry(6, 32, 16);
 let jupiterStandardMater = new THREE.MeshStandardMaterial({
 	flatShading: false,
+	color: 'white',
 	wireframe: false,
-	map: jupiterColorMap,
+	// map: jupiterColorMap,
 });
 let jupiter = new THREE.Mesh(jupiterGeo, jupiterStandardMater);
 scene.add(jupiter);
